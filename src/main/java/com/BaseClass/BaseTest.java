41b22ab4eefc568e508d2b1fd99b03f61855b04c
kz1985 <khosruzzaman.ny@gmail.com>
2023-10-16 16:10:02 -0400

    Deploy 01

---
+++ src/main/java/com/BaseClass/BaseTest.java
@@ -0,0 +1,77 @@
+package com.BaseClass;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.System.Logger;
+import java.net.URL;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import com.Capabilities.CapabilityFactory;
+import com.Utility.TestUtils;
+
+public class BaseTest {
+
+	public static Properties prop;
+	public String projectPath = System.getProperty("user.dir");
+	 
+
+	// Declare ThreadLocal Driver (ThreadLocalMap) for ThreadSafe Tests
+	public static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<>();
+	public CapabilityFactory capabilityFactory = new CapabilityFactory();
+
+	public BaseTest() {
+		try {
+			prop = new Properties();
+			FileInputStream fis = new FileInputStream(projectPath + "/" + "config.properties");
+			prop.load(fis);
+		} catch (FileNotFoundException fnfe) {
+			fnfe.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+	@BeforeMethod
+	@Parameters(value = { "browser"})
+	public void setup(String browser) throws IOException, InterruptedException {
+		// Set Browser to ThreadLocalMap
+		String envName = prop.getProperty("environment");
+			if(envName.equals("awsEnv")) {
+			driver.set(new RemoteWebDriver(new URL("http://"+prop.getProperty("awsEnv")+":4444/wd/hub"), 
+					capabilityFactory.getCapabilities(browser)));
+			getDriver().navigate().to(prop.getProperty("url"));
+		}else {
+	TestUtils.start_Docker();
+			driver.set(new RemoteWebDriver(new URL(prop.getProperty("localEnv")), // Local execution with docker
+					capabilityFactory.getCapabilities(browser)));
+			getDriver().navigate().to(prop.getProperty("url"));
+		}
+
+	}
+
+	public static WebDriver getDriver() {
+		// Get driver from ThreadLocalMap
+		return driver.get();
+	}
+
+	@AfterMethod
+	public void tearDown() throws IOException, InterruptedException {
+		getDriver().quit();
+		TestUtils.stop_Docker();
+	}
+
+	@AfterClass
+	void terminate() throws IOException, InterruptedException {
+		// Remove the ThreadLocalMap element
+		driver.remove();
+
+	}
+}
